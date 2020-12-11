04dca79c32264124128b0d23e38bcfef59de1822
Jeremy <jeremy.spewock@gmail.com>
2020-12-11 15:02:34 -0500

    set up create form component

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -4,13 +4,30 @@ export default class CreateForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      
+      title: '',
+      body: ''  
     }
+    this.handleChange = this.handleChange.bind(this)
   }
+
+  handleChange(event) {
+    this.setState({[event.target.name] : event.target.value})
+  }
+
+  handleSubmit() {
+
+  }
+
   render() {
     return (
       <div>
-        
+        <form>
+          <label htmlFor='title'>Title:</label>
+          <input name='title' type='text' onChange={this.handleChange} value={this.state.title} />
+          <label htmlFor='body'>Body:</label>
+          <input name='body' type='text' onChange={this.handleChange} value={this.state.body} />
+          <input type='submit' value='Add Post' />
+        </form>
       </div>
     )
   }
